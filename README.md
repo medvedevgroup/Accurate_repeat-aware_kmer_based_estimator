43a8e3d0677a3631eaab10a9d6f6c28230e6f106
Wu Haonan <hnw.bio@outlook.com>
2026-01-29 21:32:34 -0500

    readme update

--- README.md
+++ README.md
@@ -9,7 +9,10 @@ This tool is a prototype of three **repeat-robust**, **$k$-mer-based** estimator
 ## Installation
 
 ```bash
+# Clone the Repository
+git clone git@github.com:medvedevgroup/Accurate_repeat-aware_kmer_based_estimator.git
 # Compile the tool
+cd Accurate_repeat-aware_kmer_based_estimator
 make
 ```
 
@@ -20,13 +23,13 @@ Requirements: C++17 compatible compiler.
 ### Basic 
 
 ```bash
-mutation_estimator --mode <pp|pc|cc> -L <length> -k <kmer_size> -n <novel_kmer_count> [options]
+mutation_estimator --mode <pp|pc|cc> -m <length> -k <kmer_size> -n <novel_kmer_count> [options]
 ```
 
 ### Required arguments
 
 - `--mode <mode>`: Estimation mode (pp, pc, or cc)
-- `-L <length>`: Total sequence length in base pairs of $t$
+- `-m <length>`: Total sequence length in base pairs of $t$
 - `-k <size>`: K-mer size
 - `-n <count of novel kmers>`: Number of novel k-mers (see mode-specific details below)
 
@@ -49,8 +52,22 @@ Use distinct k-mer sets of $s$ and $t$. Suitable when you have the k-mer spectra
 
 ```bash
 # You have 10,000 distinct novel k-mers
-mutation_estimator --mode pp -L 1000000 -k 21 -n 10000
+mutation_estimator --mode pp -m 1000000 -k 21 -n 10000
 ```
+
+After running this command, you should get as output like
+
+```
+Mode: presence-to-presence (--pp)
+Parameters:
+  Sequence length -k +1: 999980
+  K-mer size: 21
+  Novel k-mers: 10000
+
+Estimated mutation rate: 0.000478
+```
+
+
 
 ### Mode 2: --pc (presence-to-count)
 
@@ -58,49 +75,48 @@ Use distinct k-mer set of $s$ and k-mer counts of $t$. More accurate than --pp w
 
 **Required input**:
 
-- `-n`: Total number of novel k-mer occurrences in t (with multiplicity)
+- `-n`: Sum of counts of k-mers in t, using only k-mers that do not occur in $s$.
 
 **Example**:
 
 ```bash
 # You have 10,000 total novel k-mer occurrences
-mutation_estimator --mode pc -L 1000000 -k 21 -n 10000
+mutation_estimator --mode pc -m 1000000 -k 21 -n 10000
 ```
 
 ### Mode 3: --cc (count-to-count)
 
-Use k-mer counts of $s$ and $t$. Most accurate mode for repetitive sequences. We need $d_1$ value of each k-mer in $s$, where $d_1$ is the number of k-mers with hamming distance $1$ in the spectrum of $s$. 
+Use k-mer counts of $s$ and $t$. Most accurate mode for repetitive sequences. We need $d_1$ value of each k-mer in $s$, where $d_1$ is the number of k-mers with hamming distance $1$ in the spectrum of $s$. For multi-record FASTA files, the code will parse each sequence separately and automatically excludes any $k$-mers containing 'N'.
 
 **Required input**:
 
-- `-n`: Total number of novel k-mer occurrences in t
-- `-s`: FASTA file containing the original sequence s
+- `-n`: Total number of novel k-mer occurrences in $t$
+- `-s`: FASTA file containing the original sequence $s$
 
 **Example**:
 
 ```bash
 # You have 10,000 total novel k-mer occurrences and original sequence s
-mutation_estimator --mode cc -L 1000000 -k 21 -n 10000 -s original.fasta
+mutation_estimator --mode cc -m 1000000 -k 21 -n 10000 -s original.fasta
 ```
 
 ## Using with FracMinHash
 
-If you count your novel kmer based on FracMinHash, use the `-t` parameter to specify the sampling rate:
+If the number for $n$ is based on comparison of FracMinHash data, then use the `-t` parameter to specify the sampling rate that you had used. This allows the estimator to add a correction factor based on theta.
 
 ```bash
 # If you sketched with theta=0.01
 # and found 150 novel sketched k-mers
-mutation_estimator --mode pp -L 1000000 -k 21 -n 150 -t 0.01
+mutation_estimator --mode pp -m 1000000 -k 21 -n 150 -t 0.01
 ```
 
 ### How to get k-mer counts
 
-You can obtain novel k-mer counts from various tools, like [KMC](https://github.com/refresh-bio/KMC), [Jellyfish](https://github.com/gmarcais/Jellyfish), or estimate by FracMinHash by using [sourmash](https://github.com/sourmash-bio/sourmash)
+Note that our tools does not do k-mer counting or sketching. For k-mer counting and computing the intersection size you can use tools like [KMC](https://github.com/refresh-bio/KMC). For sketching, you can use [sourmash](https://github.com/sourmash-bio/sourmash).
 
 ## Citation
 
 If you use this tool, please cite:
 
-```
-Haonan Wu, Paul Medvedev. The gift of creation: repeat-robust estimators of substitution rates. 2026 under review. 
-```
+Haonan Wu and Paul Medvedev, *The gift of creation: repeat-robust estimators of substitution rates*, 2026, under review.
+
--- mutation_estimator.cpp
+++ mutation_estimator.cpp
@@ -16,7 +16,7 @@ void print_usage() {
          << "Usage: mutation_estimator [options]\n\n"
          << "Required arguments:\n"
          << "  --mode <mode>      Estimation mode: pp, pc, or cc\n"
-         << "  -L <length>        Sequence length (number of positions)\n"
+         << "  -m <length>        Sequence length (number of base pairs)\n"
          << "  -k <kmer_size>     K-mer size\n\n"
          << "Mode-specific arguments:\n"
          << "  --pp mode (presence-to-presence):\n"
@@ -29,16 +29,16 @@ void print_usage() {
          << "Optional arguments:\n"
          << "  -t <theta>         Sketch rate (default: 1.0, no sketching)\n"
          << "                     Use this if your novel k-mer count comes from\n"
-         << "                     FracMinHash or other sketching methods\n\n"
+         << "                     FracMinHash\n\n"
          << "Examples:\n"
          << "  # Using distinct k-mers of t(pp mode)\n"
-         << "  mutation_estimator --mode pp -L 1000000 -k 21 -n 15000\n\n"
+         << "  mutation_estimator --mode pp -m 1000000 -k 21 -n 15000\n\n"
          << "  # Using k-mer counts of t (pc mode)\n"
-         << "  mutation_estimator --mode pc -L 1000000 -k 21 -n 18000\n\n"
+         << "  mutation_estimator --mode pc -m 1000000 -k 21 -n 18000\n\n"
          << "  # Using k-mer counts of s and t (cc mode)\n"
-         << "  mutation_estimator --mode cc -L 1000000 -k 21 -n 18000 -s seq.fasta\n\n"
+         << "  mutation_estimator --mode cc -m 1000000 -k 21 -n 18000 -s seq.fasta\n\n"
          << "  # With FracMinHash sketch (theta = 0.01)\n"
-         << "  mutation_estimator --mode pc -L 1000000 -k 21 -n 180 -t 0.01\n";
+         << "  mutation_estimator --mode pc -m 1000000 -k 21 -n 180 -t 0.01\n";
 }
 
 // Structure to hold input parameters
@@ -54,8 +54,13 @@ struct EstimatorInput {
     int novel_multi;    // Number of novel kmers with multiplicity
     
     // For --cc mode only
-    string s_sequence;  // Original sequence s
+    vector<string> s_sequences;  // Original sequences from FASTA
 };
+
+// Check if a k-mer contains N
+bool contains_N(const string& kmer) {
+    return kmer.find('N') != string::npos;
+}
 
 // Compute h1 values for all kmers efficiently using masked hash table
 unordered_map<string, int> compute_h1_values(const set<string>& kmer_set, int k) {
@@ -91,28 +96,46 @@ unordered_map<string, int> compute_h1_values(const set<string>& kmer_set, int k)
     return h1_map;
 }
 
-// Get k-spectrum from sequence
+// Get k-spectrum from sequence, excluding k-mers with N
 set<string> kspectrum(const string& s, int k) {
     set<string> kmers;
     for (size_t i = 0; i <= s.length() - k; i++) {
-        kmers.insert(s.substr(i, k));
+        string kmer = s.substr(i, k);
+        if (!contains_N(kmer)) {
+            kmers.insert(kmer);
+        }
     }
     return kmers;
 }
 
-// Count kmer occurrences
+// Count kmer occurrences, excluding k-mers with N
 unordered_map<string, int> kmer_counter(const string& s, int k) {
     unordered_map<string, int> kmer_count;
     for (size_t i = 0; i <= s.length() - k; i++) {
-        kmer_count[s.substr(i, k)]++;
+        string kmer = s.substr(i, k);
+        if (!contains_N(kmer)) {
+            kmer_count[kmer]++;
+        }
     }
     return kmer_count;
 }
 
-// Compute sum of occurrence * h1 for all kmers
-double compute_sum_occ_h1(const string& s, int k) {
-    set<string> kmer_set = kspectrum(s, k);
-    unordered_map<string, int> kmer_count = kmer_counter(s, k);
+// Compute sum of occurrence * h1 for all kmers across all sequences
+double compute_sum_occ_h1(const vector<string>& sequences, int k) {
+    // Collect all k-mers from all sequences
+    set<string> kmer_set;
+    unordered_map<string, int> kmer_count;
+    
+    for (const auto& s : sequences) {
+        set<string> seq_kmers = kspectrum(s, k);
+        kmer_set.insert(seq_kmers.begin(), seq_kmers.end());
+        
+        unordered_map<string, int> seq_counts = kmer_counter(s, k);
+        for (const auto& [kmer, count] : seq_counts) {
+            kmer_count[kmer] += count;
+        }
+    }
+    
     unordered_map<string, int> h1_map = compute_h1_values(kmer_set, k);
     
     double sum_occ_h1 = 0.0;
@@ -188,8 +211,8 @@ double estimate_r_cc(const EstimatorInput& input) {
     }
     double r_sm = 1.0 - pow(1.0 - q_sm, 1.0 / input.k);
     
-    // Compute sum_occ_h1 from s sequence
-    double sum_occ_h1 = compute_sum_occ_h1(input.s_sequence, input.k);
+    // Compute sum_occ_h1 from all sequences
+    double sum_occ_h1 = compute_sum_occ_h1(input.s_sequences, input.k);
     
     // Compute q_strong
     double correction = sum_occ_h1 * pow(1.0 - r_sm, input.k - 1) * r_sm / 3.0;
@@ -204,27 +227,41 @@ double estimate_r_cc(const EstimatorInput& input) {
     return r_strong;
 }
 
-string read_fasta(const string& filename) {
+// Read all sequences from FASTA file
+vector<string> read_fasta(const string& filename) {
     ifstream file(filename);
     if (!file.is_open()) {
         cerr << "Error: Cannot open file " << filename << endl;
         exit(1);
     }
     
+    vector<string> sequences;
     string line;
-    string sequence;
+    string current_sequence;
     
     while (getline(file, line)) {
         if (line.empty()) continue;
-        if (line[0] == '>') continue;
         
-        // Convert to uppercase
-        transform(line.begin(), line.end(), line.begin(), ::toupper);
-        sequence += line;
+        if (line[0] == '>') {
+            // Save previous sequence if exists
+            if (!current_sequence.empty()) {
+                sequences.push_back(current_sequence);
+                current_sequence.clear();
+            }
+        } else {
+            // Convert to uppercase and append
+            transform(line.begin(), line.end(), line.begin(), ::toupper);
+            current_sequence += line;
+        }
+    }
+    
+    // Don't forget the last sequence
+    if (!current_sequence.empty()) {
+        sequences.push_back(current_sequence);
     }
     
     file.close();
-    return sequence;
+    return sequences;
 }
 
 int main(int argc, char* argv[]) {
@@ -240,7 +277,7 @@ int main(int argc, char* argv[]) {
     input.theta = 1.0;
     input.novel_spectrum = -1;
     input.novel_multi = -1;
-    input.s_sequence = "";
+    // s_sequences is a vector, no need to initialize
     string s_file = "";
     
     // Parse arguments
@@ -256,7 +293,7 @@ int main(int argc, char* argv[]) {
                 mode = argv[++i];
             }
         }
-        else if (arg == "-L") {
+        else if (arg == "-m") {
             if (i + 1 < argc) {
                 input.L = stoi(argv[++i]);
             }
@@ -293,7 +330,7 @@ int main(int argc, char* argv[]) {
     }
     
     if (input.L <= 0) {
-        cerr << "Error: Sequence length -L must be positive\n";
+        cerr << "Error: Sequence length -m must be positive\n";
         return 1;
     }
     
@@ -336,13 +373,29 @@ int main(int argc, char* argv[]) {
     // Convert L to effective length (L - k + 1)
     input.L = input.L - input.k + 1;
     
-    // Load sequence for cc mode
+    // Load sequences for cc mode
     if (mode == "cc") {
-        input.s_sequence = read_fasta(s_file);
-        if (input.s_sequence.length() < static_cast<size_t>(input.k)) {
-            cerr << "Error: Sequence in " << s_file << " is shorter than k-mer size\n";
+        input.s_sequences = read_fasta(s_file);
+        if (input.s_sequences.empty()) {
+            cerr << "Error: No sequences found in " << s_file << "\n";
             return 1;
         }
+        
+        // Check if any sequence is too short
+        bool has_short_seq = false;
+        for (const auto& seq : input.s_sequences) {
+            if (seq.length() < static_cast<size_t>(input.k)) {
+                has_short_seq = true;
+                break;
+            }
+        }
+        
+        if (has_short_seq) {
+            cerr << "Warning: Some sequences in " << s_file 
+                 << " are shorter than k-mer size and will be skipped for those positions\n";
+        }
+        
+        cout << "Loaded " << input.s_sequences.size() << " sequence(s) from " << s_file << "\n";
     }
     
     // Compute mutation rate estimate
